5ae15f2ff79dfdec74ccf06b973d141c9e677fa3
wjw941109 <2459604359@qq.com>
2019-04-11 16:13:52 +0800

    add js选项卡与timeAndRegExp

---
+++ js选项卡搜索正则/js/main.js
@@ -0,0 +1,21 @@
+// let regist = document.getElementsByClassName('login-regist');
+// console.log(regist);
+// let b = regist.children();
+// console.log(b);
+// function regist(){
+//     let regist_div =document.getElementsByClassName('regist_div');
+//     console.log(regist_div)
+//     for (let i = 0; i < regist_div.length; i++) {
+//         regist_div[i].style.display='block'
+//         console.log(regist_div[i])
+//     }
+// }
+
+
+// let a = document.getElementsByClassName('list')
+        
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i])
+//     a[i].style.color='red';
+    
+// }
